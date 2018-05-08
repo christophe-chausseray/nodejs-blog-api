8a2bf744e8e0d75637f6c1a2cf5498140cfc7c2c
christophe-chausseray <chausseray.christophe@gmail.com>
2018-05-08 16:11:31 +0200

    [Fix] Update edit article handler to throw the find article error in the repository.

--- src/library/application/useCase/command/editArticleHandler.js
+++ src/library/application/useCase/command/editArticleHandler.js
@@ -1,4 +1,3 @@
-const ArticleNotFound = require('src/library/domain/exceptions/articleNotFound');
 const Article = require('src/library/domain/model/article');
 
 class EditArticleHandler {
@@ -14,16 +13,10 @@ class EditArticleHandler {
   /**
    * Handle the article's edition
    *
-   * @throws {ArticleNotFound} When the article wasn't found and can't be updated.
-   *
    * @param {EditArticle} editArticle
    */
   async handle({ slug, params }) {
     const article = await this.mongoArticleRepository.findOneAndUpdate(slug, params);
-
-    if (article === null) {
-      throw ArticleNotFound.withSlug(slug);
-    }
 
     return Article.create(article.slug, article.title, article.description, article.body);
   }
